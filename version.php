65779236cf88ac36840c04ae5e48186ff032ab0e
David Scotson <dscotson@moodlerooms.com>
2015-08-31 17:05:45 +0100

    INT-7524: mark as stable release

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_collaborate';
 $plugin->version = 2015072400;
-$plugin->release = 'v0.0';
+$plugin->release = 'v1.0';
 $plugin->requires = 2014051200;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
 $plugin->dependencies = array();
