020f9a02fee90a76c98d64e409486f37010ccc6d
Mark Nielsen <mark@moodlerooms.com>
2017-12-08 15:42:58 -0800

    INT-12724: Bump version and release

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_collaborate';
-$plugin->version = 2017111000;
-$plugin->release = '3.3.1';
+$plugin->version = 2017120800;
+$plugin->release = '3.3.3';
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
