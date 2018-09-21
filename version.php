97d599171c302b8c7e6b4f8178edc1994b2fd8b4
David Castro <david.castro@blackboard.com>
2018-09-21 15:23:26 -0500

    INT-13685: Bump requires and release for Moodle 3.5.2

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_collaborate';
 $plugin->version = 2018080800;
-$plugin->release = '3.4.3';
-$plugin->requires = 2017111300;
+$plugin->release = '3.5.2';
+$plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
 $plugin->dependencies = array();
