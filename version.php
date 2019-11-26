ced99ffba90bc79cc77ad5a44325ea71809b9837
Jonathan Garcia <jonathan.garcia@blackboard.com>
2019-11-26 15:31:23 -0500

    INT-15220: Version and required version bump

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_collaborate';
 $plugin->version = 2018080800;
-$plugin->release = '3.6.3';
-$plugin->requires = 2018120300;
+$plugin->release = '3.7.3';
+$plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
 $plugin->dependencies = array();
