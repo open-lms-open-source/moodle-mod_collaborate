3840c32a0304a5fc3bdbb7ede6b7052826089da1
Rafael Monterroza <rafael.monterroza@openlms.net>
2022-07-29 18:07:24 +0000

    INT-18181: bump versions for 3.11 mp4

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_collaborate';
-$plugin->version = 2022060600;
-$plugin->release = '3.11.3';
+$plugin->version = 2022072900;
+$plugin->release = '3.11.6';
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
