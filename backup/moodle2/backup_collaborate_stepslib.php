454563ba0d50b79efeac00bfdfd68d72c2ce8360
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2023-08-22 17:34:17 -0500

    INT-19197 mod_collaborate: Fix duplicated Collaborate activity to inherit source Instructor Settings

--- backup/moodle2/backup_collaborate_stepslib.php
+++ backup/moodle2/backup_collaborate_stepslib.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - https://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,27 +12,27 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
- * Define all the backup steps that will be used by the backup_collaborate_activity_task
+ * Define all the backup steps that will be used by the {@see backup_collaborate_activity_task}.
  *
  * @package   mod_collaborate
  * @copyright Copyright (c) 2015 Open LMS (https://www.openlms.net)
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license   https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 /**
- * Define the complete collaborate structure for backup, with file and id annotations
+ * Define the complete collaborate structure for backup, with file and id annotations.
  *
  * @package   mod_collaborate
  * @copyright Copyright (c) 2015 Open LMS (https://www.openlms.net)
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license   https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class backup_collaborate_activity_structure_step extends backup_activity_structure_step {
 
     /**
-     * Defines the backup structure of the module
+     * Defines the backup structure of the module.
      *
      * @return backup_nested_element
      */
@@ -42,7 +42,9 @@ class backup_collaborate_activity_structure_step extends backup_activity_structu
         $collaborate = new backup_nested_element('collaborate', array('id'), array(
             'name', 'intro', 'introformat', 'grade', 'timestart', 'timeend',
             'duration', 'boundaryminutes', 'sessionid', 'completionlaunch',
-            'guestaccessenabled', 'guestrole', 'guesturl',
+            'guestaccessenabled', 'guestrole', 'guesturl', 'canpostmessages',
+            'canannotatewhiteboard', 'cansharevideo', 'canshareaudio',
+            'candownloadrecordings', 'largesessionenable',
         ));
 
         // If we had more elements, we would build the tree here.
@@ -50,8 +52,8 @@ class backup_collaborate_activity_structure_step extends backup_activity_structu
         // Define data sources.
         $collaborate->set_source_table('collaborate', array('id' => backup::VAR_ACTIVITYID));
 
-        // If we were referring to other tables, we would annotate the relation
-        // with the element's annotate_ids() method.
+        // If we were referring to other tables, we would annotate
+        // the relation with the element's annotate_ids() method.
 
         // Define file annotations (we do not use itemid in this example).
         $collaborate->annotate_files('mod_collaborate', 'intro', null);
