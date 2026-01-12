2f4112d918690894507260f69e577c3c2486cce4
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2026-01-12 02:20:03 -0500

    INT-21481: 4.5 MP4 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_collaborate';
-$plugin->version = 2025102900;
+$plugin->version = 2026011300;
 $plugin->release = '4.5.4';
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
